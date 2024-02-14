e58183004e78ade862527cd2339b6908df0d5fa0
Steeven Bachet <steevy72500@gmail.com>
2024-02-14 13:39:01 +0100

    ajout des boutons de filtrage

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,30 +1,65 @@
-console.log("yoyo")
-
 async function app() {
     const reponse = await fetch("http://localhost:5678/api/works");
     const travaux = await reponse.json();
     afficherTravaux(travaux);
+    afficherElementsFiltres(travaux)
 }
+
+app()
+
+const galerie = document.querySelector(".gallery");
 
 function afficherTravaux(travaux) {
 
-    const galerie = document.querySelector(".gallery");
+    
     
     for(const travail of travaux) {
         const figure = document.createElement("figure");
+
         const image = document.createElement("img");
         image.src = travail.imageUrl;
         image.alt = travail.title;
 
+        const figcaption = document.createElement("figcaption");
+        figcaption.textContent = travail.title
 
-        /* créer le figcaption */
+
         figure.appendChild(image);
+        figure.appendChild(figcaption)
         galerie.appendChild(figure);
         
-        console.log(travail);
+    }
+    
+}
+
+afficherElementsFiltres = (travaux) => {
+
+    const boutons = document.querySelectorAll(".filtres button");
+
+    for(let bouton of boutons) {
+        bouton.addEventListener("click", () => {
+            if(bouton.value === "0") {
+                galerie.innerHTML = ""
+                afficherTravaux(travaux)
+            }
+            else {
+                const travauxFiltres = travaux.filter(function (travail) {
+                    return travail.categoryId === parseInt(bouton.value);
+                })
+                galerie.innerHTML = ""
+                afficherTravaux(travauxFiltres)
+            }
+
+            /* Couleur des boutons */ 
+
+            boutons.forEach((bouton) => {
+                bouton.classList.remove("active");
+            });
+            bouton.classList.add("active");
+            
+        })
     }
 
-    debugger
 }
 
 
@@ -34,4 +69,11 @@ function afficherTravaux(travaux) {
 
 
 
-app()
+
+
+
+
+
+
+
+
